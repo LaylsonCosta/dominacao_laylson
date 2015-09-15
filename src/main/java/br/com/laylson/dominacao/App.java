3fc6b9d6ca09da693486f42b17c0e72a971886a2
Laylson Costa <laylsonehnoix@gmail.com>
2015-09-15 14:03:23 -0300

    Meu primeiro commit

---
+++ src/main/java/br/com/laylson/dominacao/App.java
@@ -0,0 +1,13 @@
+package br.com.laylson.dominacao;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
