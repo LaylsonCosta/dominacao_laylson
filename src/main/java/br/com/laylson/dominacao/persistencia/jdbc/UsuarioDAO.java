4f1597c0df74efb1fbea42c7a9a6a42e12f61327
Laylson Costa <laylsonehnoix@gmail.com>
2015-09-16 00:26:34 -0300

    conclusão das classes DAO

--- src/main/java/br/com/laylson/dominacao/persistencia/jdbc/UsuarioDAO.java
+++ src/main/java/br/com/laylson/dominacao/persistencia/jdbc/UsuarioDAO.java
@@ -2,7 +2,9 @@ package br.com.laylson.dominacao.persistencia.jdbc;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 
 import br.com.laylson.dominacao.persistencia.entidade.Usuario;
 
@@ -72,6 +74,102 @@ public class UsuarioDAO {
 		
 	}
 		
-	
+	public void salvar(Usuario usu){
+		if (usu.getId()!=null){
+			alterar(usu);
+		}else {
+			cadastrar(usu);
+			
+		}
+		
+	}
+	/**
+	 * Busca de um registro no banco de dados pelo numero do id do usuário
+	 * @param id é um inteiro que reprensenta o número do id do usuário a ser buscado
+	 * @return Um objeto usuário quando encontra ou null quando não encontra
+	 */
+	public Usuario buscarPorId(Integer id){
+		String sql = "select * from usuario where id =?";
+		
+		try(PreparedStatement pst = con.prepareStatement(sql)){
+			pst.setInt(1, id);
+			ResultSet resultado = pst.executeQuery();
+			//posicionado cursos no primeiro registro
+			if(resultado.next()){//mesma coisa que: if(resultado.next()==true);
+				Usuario usuario = new Usuario();
+				usuario.setId(resultado.getInt("id"));
+				usuario.setNome(resultado.getString("nome"));
+				usuario.setLogin(resultado.getString("login"));
+				usuario.setSenha(resultado.getString("senha"));
+				
+				return usuario;
+			}
+			
+		} catch (SQLException e) {
 
+			e.printStackTrace();
+		}
+		return null;
+		
+	}
+	/**
+	 * Realiza a busca de todos resgristros da tabela de usuário
+	 * @return Uma lista de objetos Usuario contendo 0 elementos quando não tiver registro
+	 * ou n elementos quando tiver resultado
+	 */
+	
+	
+	public java.util.List<Usuario> buscarTodos(){
+		
+		String sql = "select * from usuario";
+		java.util.List<Usuario> lista = new ArrayList<Usuario>();
+		try(PreparedStatement pst = con.prepareStatement(sql)){
+			
+			ResultSet resultado = pst.executeQuery();
+			//posicionado cursos no primeiro registro
+			while(resultado.next()){//mesma coisa que: while(resultado.next()==true);
+				Usuario usuario = new Usuario();
+				usuario.setId(resultado.getInt("id"));
+				usuario.setNome(resultado.getString("nome"));
+				usuario.setLogin(resultado.getString("login"));
+				usuario.setSenha(resultado.getString("senha"));
+				//Adicionando usuario na lista
+				lista.add(usuario);
+			}
+			
+		} catch (SQLException e) {
+
+			e.printStackTrace();
+		}
+		return lista;
+		
+	}
+	
+	public Usuario autenticar(Usuario usuConcuslta){
+		
+		String sql = "select * from usuario where login=? and senha=?";
+		
+		try(PreparedStatement pst = con.prepareStatement(sql)){
+			pst.setString(1, usuConcuslta.getLogin());
+			pst.setString(2, usuConcuslta.getSenha());
+			ResultSet resultado = pst.executeQuery();
+			
+			if(resultado.next()){
+				Usuario usuario = new Usuario();
+				usuario.setId(resultado.getInt("id"));
+				usuario.setNome(resultado.getString("nome"));
+				usuario.setLogin(resultado.getString("login"));
+				usuario.setSenha(resultado.getString("senha"));
+				
+				return usuario;
+				
+			}
+			
+		} catch (SQLException e) {
+			
+			e.printStackTrace();
+		}
+		return null;
+	}
+	
 }
--- src/test/java/br/com/laylson/dominacao/TesteUsuarioDAO.java
+++ src/test/java/br/com/laylson/dominacao/TesteUsuarioDAO.java
@@ -1,19 +1,49 @@
 package br.com.laylson.dominacao;
 
+import java.util.List;
+
 import br.com.laylson.dominacao.persistencia.entidade.Usuario;
 import br.com.laylson.dominacao.persistencia.jdbc.UsuarioDAO;
 
 public class TesteUsuarioDAO {
 
+	private static Usuario autenticar;
 	public static void main(String[] args) {
-		testeCadastrar();
+		//testeCadastrar();
+		//testeSalvar();
+		//testeBuscarTodos();
+		testeAutenticar();
+	}
+	private static void testeAutenticar() {
+		UsuarioDAO usuarioDAO = new UsuarioDAO();
+		Usuario usuario = new Usuario();
+		usuario.setLogin("laylson");
+		usuario.setSenha("laylson123");
+		
+		Usuario usuRetorno = usuarioDAO.autenticar(usuario);
+		System.out.println(usuRetorno);
 		
 	}
+	private static void testeBuscarPorId() {
+		UsuarioDAO usuarioDAO = new UsuarioDAO();
+		Usuario usuario = usuarioDAO.buscarPorId(1);
+		System.out.println(usuario);
+		
+	}
+	
+	private static void testeBuscarTodos() {
+		UsuarioDAO usuarioDAO = new UsuarioDAO();
+		List<Usuario> lista = usuarioDAO.buscarTodos();
+		for (Usuario u: lista){
+		System.out.println(u);
+		}
+	}
+	
 	public static void testeAlterar(){
-		//Criando Usuário
+		//Alterando Usuário
 		Usuario usu = new Usuario();
 		usu.setId(4);
-		usu.setNome("jaoalterado");
+		usu.setNome("");
 		usu.setLogin("jaologinalteradp");
 		usu.setSenha("ja0123alterado");
 		
@@ -28,9 +58,9 @@ public class TesteUsuarioDAO {
 	public static void testeCadastrar(){
 		//Criando Usuário
 		Usuario usu = new Usuario();
-		usu.setNome("jao");
-		usu.setLogin("jaologin");
-		usu.setSenha("ja0123");
+		usu.setNome("laylson");
+		usu.setLogin("laylson");
+		usu.setSenha("laylson123");
 		
 		//Cadastrando usuário no banco de dados
 		UsuarioDAO usuDAO = new UsuarioDAO();
@@ -41,7 +71,7 @@ public class TesteUsuarioDAO {
 		
 	}
 	public static void testeDeletar(){
-		//Criando Usuário
+		//defininso Usuário a ser deletado
 		Usuario usu = new Usuario();
 		usu.setId(1);
 		
@@ -53,4 +83,18 @@ public class TesteUsuarioDAO {
 		
 		
 	}
+	public static void testeSalvar(){
+		Usuario usu = new Usuario();
+		//usu.setId(5);
+		usu.setNome("testedoSalvar");
+		usu.setLogin("testedoSalvarLogin");
+		usu.setSenha("testedoSalvar123");
+		
+		UsuarioDAO usuDAO = new UsuarioDAO();
+		usuDAO.salvar(usu);
+		
+		System.out.println("Salvo com sucesso!!!");
+	}
+	
+	
 }
